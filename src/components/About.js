0238355606eebf65a3ed3f0e992456e11f11fb21
bnsl-balwinder <balwinder@bnsellit.com>
2023-10-11 22:17:59 -0400

    fixed about us page

--- src/components/About.js
+++ src/components/About.js
@@ -4,39 +4,40 @@ import AboutImg from "../assets/about-img.png";
 const About = () => {
   return (
     <section className="bg-secondery text-white px-5 py-32" id="about">
-      <div className="container mx-auto grid md:grid-cols-2 items-center justify-center md:justify-between">
+    <div className="container mx-auto grid md:grid-cols-2 items-center justify-center md:justify-between">
         <div className="about-info">
-          <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[180px] border-indigo-600 pb-2">
-            About Me
-          </h2>
+            <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[180px] border-indigo-600 pb-2">
+                About Me
+            </h2>
 
-          <p className="pb-5">
-            Hi, My Name Is Rohima Akther everyone calls me Amrin. I am a
-            Frontend Developer. I build beautifull websites with React and
-            Tailwind CSS.
-          </p>
-          <p className="pb-5">
-            I am proficient in Frontend skills like React.js, Redux, Redux Tool
-            Kit, Axios, Tailwind CSS, SaSS, Css3 and many more.
-          </p>
+            <p className="pb-5">
+                Hi, My Name Is Balwinder Singh and everyone calls me Balwinder. I am a
+                Software Developer specialized in mobile and web application development.
+            </p>
+            
+            <p className="pb-5">
+                I am adept in building mobile applications using React Native for both iOS and Android platforms. My expertise in web development is backed by my knowledge of React.js, enriched by the power of Node.js for the backend support.
+            </p>
 
-          <p>In backend I know Node.js, Express.js, MongoDB, and Mongoose</p>
+            <p>
+                I am also proficient with MongoDB for handling databases, ensuring seamless data management and operations. My skill set also spans across Redux, Redux Tool Kit, Axios, Tailwind CSS, SaSS, and Css3 among others.
+            </p>
 
-          <p>
-            In my spare time I create YouTube videos and write blogs on my Blog.
-            Where I talk about programming theory and build various projects.
-          </p>
+            <p>
+            When not coding, I enjoy conceptualizing and creating code structures. It allows me to visualize and better understand complex coding paradigms and scenarios.
+            </p>
         </div>
 
         <div className="about-img">
-          <img
-            src={AboutImg}
-            alt="coding illustration"
-            className="lgw-[80%] md:ml-auto"
-          />
+            <img
+                src={AboutImg}
+                alt="coding illustration"
+                className="lgw-[80%] md:ml-auto"
+            />
         </div>
-      </div>
-    </section>
+    </div>
+</section>
+
   );
 };
 
